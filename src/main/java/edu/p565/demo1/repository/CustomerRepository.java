a8a9d7a883ddf37bce15797c4bac2dfe690fc485
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-04-06 16:53:10 -0400

    .

--- src/main/java/edu/p565/demo1/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.p565.demo1.repository;
-
-import edu.p565.demo1.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
